30601b2f5496509c7ddc2aa027ed25480b922a29
Ibrahim Shaker <shaker.personnel@gmail.com>
2020-08-31 13:09:29 +0200

    adding authentication with firebase

---
+++ screens/user/AuthScreen.js
@@ -0,0 +1,190 @@
+import React, { useState, useEffect, useReducer, useCallback } from "react";
+import {
+  ScrollView,
+  View,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useDispatch } from "react-redux";
+
+import Input from "../../components/UI/Input";
+import Card from "../../components/UI/Card";
+import Colors from "../../constants/Colors";
+import { signup, login } from "../../store/actions/authA";
+
+const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE";
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.inputIdentifier]: action.value,
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.inputIdentifier]: action.isValid,
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      isFormValid: updatedFormIsValid,
+      inputValues: updatedValues,
+      inputValidities: updatedValidities,
+    };
+  }
+  return state;
+};
+
+const AuthScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const [error, setError] = useState();
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      email: "",
+      password: "",
+    },
+    inputValidities: {
+      email: false,
+      password: false,
+    },
+    isFormValid: false,
+  });
+
+  useEffect(() => {
+    if (error) {
+      Alert.alert("An error occurred!", error, [{ text: "Okay" }]);
+    }
+  }, [error]);
+
+  const inputChangeHandler = useCallback(
+    (inputIdentifier, value, isValid) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE,
+        value,
+        isValid,
+        inputIdentifier,
+      });
+    },
+    [dispatchFormState]
+  );
+
+  const dispatch = useDispatch();
+
+  const authHndler = async () => {
+    let action;
+    if (isSignup) {
+      action = signup(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    } else {
+      action = login(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    }
+    setError(null);
+    setIsLoading(true);
+    try {
+      await dispatch(action);
+    } catch (err) {
+      setError(err.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior="height"
+      keyboardVerticalOffset={50}
+      style={styles.screen}
+    >
+      <LinearGradient
+        colors={["white", Colors.primary, Colors.accent]}
+        style={styles.gradient}
+      >
+        <Card style={styles.authContainer}>
+          <ScrollView>
+            <Input
+              id="email"
+              label="E-Mail"
+              keybordType="email-address"
+              required
+              email
+              autoCapitalize="none"
+              errorText="Please enter a valid email address."
+              onInputChange={inputChangeHandler}
+              initialValue=""
+            />
+            <Input
+              id="password"
+              label="Password"
+              keybordType="default"
+              secureTextEntry
+              required
+              minLength={6}
+              autoCapitalize="none"
+              errorText="Password should be at least 6 characters."
+              onInputChange={inputChangeHandler}
+              initialValue=""
+            />
+            <View style={styles.buttonContainer}>
+              {isLoading ? (
+                <ActivityIndicator size="small" color={Colors.accent} />
+              ) : (
+                <Button
+                  title={isSignup ? "SIGN UP" : "LOGIN"}
+                  color={Colors.accent}
+                  onPress={authHndler}
+                />
+              )}
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                title={`Go To ${isSignup ? "LOGIN" : "SIGN UP"}`}
+                color={Colors.primary}
+                onPress={() => {
+                  setIsSignup((prevState) => !prevState);
+                }}
+              />
+            </View>
+          </ScrollView>
+        </Card>
+      </LinearGradient>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  authContainer: {
+    width: "80%",
+    maxWidth: 400,
+    maxHeight: 400,
+    padding: 20,
+  },
+  gradient: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+});
+
+AuthScreen.navigationOptions = {
+  headerTitle: "Login",
+};
+
+export default AuthScreen;
---
+++ store/actions/authA.js
@@ -0,0 +1,72 @@
+export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
+
+export const signup = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDzVMdMpN-gs-fiMdNTcYb4JWrFNdVILZY",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      let message = "Something went wrong!";
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      if (errorId === "EMAIL_EXISTS") {
+        message = "This email is exist already!";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDzVMdMpN-gs-fiMdNTcYb4JWrFNdVILZY",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      let message = "Something went wrong!";
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      if (errorId === "EMAIL_NOT_FOUND") {
+        message = "Your email is incorrect!";
+      } else if (errorId === "INVALID_PASSWORD") {
+        message = "Invalid Password";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+
+    dispatch({ type: LOGIN });
+  };
+};
